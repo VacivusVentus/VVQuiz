554d5bb180007e3ab7ef0eef106ff4ea3e1cce62
VacivusVentus <vacivusventus@gmail.com>
2019-02-02 03:14:38 +0300

    update 00

--- VVQuiz/Source.cpp
+++ VVQuiz/Source.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include <windows.h> 
-#include <gl/GL.h>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+
+
+static int joysticks[GLFW_JOYSTICK_LAST + 1] = {0};
+static int joystick_count = 0;
+
+static void joysticProc(int jid, int event)
+{
+}
 
 int  main()
 {
@@ -10,7 +17,26 @@ int  main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	auto window = glfwCreateWindow(800, 600, "VVQuiz", 0, 0);
-    while (!glfwWindowShouldClose(window))
+	if (!window) return 1;
+
+	int jid, hat_buttons = GLFW_FALSE;
+
+	for (jid = GLFW_JOYSTICK_1; jid <= GLFW_JOYSTICK_LAST; jid++)
+	{
+		if (glfwJoystickPresent(jid))
+			joysticks[joystick_count++] = jid;
+	}
+	glfwSetJoystickCallback(joysticProc);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	for (;;)
+	{
+		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+		glClearColor(0.1,0.0,.0,1.0);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
+		if (glfwWindowShouldClose(window)) break;
+	}
+	glfwTerminate();
     return 0;
 }
